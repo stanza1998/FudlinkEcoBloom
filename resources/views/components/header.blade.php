76a1906e8e22f867705784accbb5ab834c8abed1
stanza1998 <narib98jerry@gmail.com>
2024-02-21 23:59:07 +0200

    navbar-update

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,5 @@
-<header style="background-color: #fff; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
+<header
+    style="background-color: #fff; border-bottom: solid #0b6e1f 3px; border-top: solid #0b6e1f 3px; border-bottom-right-radius: 20px; border-bottom-left-radius: 20px; border-top-right-radius: 20px; border-top-left-radius: 20px;">
     <nav style="display: flex; justify-content: space-between; align-items: center; color: #000; padding: 0 20px;">
         <a class="logo-link d-b" href="{{ route('home') }}" style="text-decoration: none; color: #000;">
             <img class="d-b" src="{{ asset('img/logo-3.png') }}" alt="user">
@@ -7,9 +8,9 @@
         <form class="ml-auto one-form" action="{{ route('shop') }}" method="GET"
             style="display: flex; align-items: center;">
             <input type="search" name="search" placeholder="Product name....." value="{{ $search ?? '' }}"
-                style="padding: 8px; border-radius: 5px; border: 1px solid #000; margin-right: 10px; background-color: transparent; color: #000; max-width: 200px;">
+                style="padding: 8px; border-radius: 5px; border: 1px solid #0b6e1f; margin-right: 10px; background-color: transparent; color: #000; max-width: 200px;">
             <button type="submit"
-                style="background-color: #000; border: none; padding: 8px; border-radius: 5px; cursor: pointer; color: #fff;">
+                style="background-color: #0b6e1f; border: none; padding: 8px; border-radius: 5px; cursor: pointer; color: #fff;">
                 Search
             </button>
         </form>
@@ -36,14 +37,14 @@
                     @if ($product_count = auth()->user()->carts()->firstOrCreate(['status' =>
                     'N'])->products->sum('pivot.quantity'))
                     <span class='badge'
-                        style="background-color: red; color: #fff; border-radius: 50%; padding: 4px 8px; position: absolute; top: -5px; right: -5px;">{{ $product_count }}</span>
+                        style="background-color: #0b6e1f; color: #fff; border-radius: 100%; padding: 2px 6px; position: absolute; top: -5px; right: -5px;">{{ $product_count }}</span>
                     @endif
                     @endauth
                     @guest
                     @if(request()->cookie('products') && $product_count =
                     array_sum(json_decode(request()->cookie('products'), true)))
                     <span class='badge'
-                        style="background-color: red; color: #fff; border-radius: 50%; padding: 4px 8px; position: absolute; top: -5px; right: -5px;">{{ $product_count }}</span>
+                        style="background-color: #0b6e1f; color: #fff; border-radius: 100%; padding: 2px 6px; position: absolute; top: -5px; right: -5px;">{{ $product_count }}</span>
                     @endif
                     @endguest
                 </a>
